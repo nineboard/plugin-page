2dce77ae3091fa9f17503646edf23378c516cf28
bnu <bnufactory@gmail.com>
2016-03-31 15:46:11 +0900

    변경된 디자인 적용

--- views/menuType/menuEdit.blade.php
+++ views/menuType/menuEdit.blade.php
@@ -1,33 +1,34 @@
-<div class="panel menu_detail">
-    <div class="panel-heading">
-        <div class="row">
-            <p class="text-title">Page 의 기본적인 설정을 입력합니다.</p>
-            <div class="right_btn">
-                <button class="ico_gray"><i class="xi-angle-down"></i><span class="blind">닫기</span></button>
-            </div>
-        </div>
+
+<div class="panel-heading">
+    <div class="pull-left">
+        <h3 class="panel-title">
+            Page 설정<small>Page의 기본적인 설정을 입력합니다</small>
+        </h3>
     </div>
+    <div class="pull-right">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse-page-options" class="btn-link panel-toggle pull-right"><i class="xi-angle-down"></i><i class="xi-angle-up"></i><span class="sr-only">메뉴닫기</span></a>
+    </div>
+</div>
+
+<div id="collapse-page-options" class="panel-collapse collapse in">
     <div class="panel-body">
-        <div class="row v2">
-            <dl>
-                <dt>Comment</dt>
-                <dd>
-                    <select name="comment" class="form-control">
-                        <option value="true" {{($config->get('comment') == true) ? 'selected="selected"' : ''}} >Use</option>
-                        <option value="false" {{($config->get('comment') == false) ? 'selected="selected"' : ''}}>Disuse</option>
-                    </select>
-                </dd>
-            </dl>
-            <dl>
-                <dt>Mobile Content</dt>
-                <dd>
-                    <select name="mobile" class="form-control">
-                        <option value="true" {{($config->get('mobile') == true) ? 'selected="selected"' : ''}} >Use</option>
-                        <option value="false" {{($config->get('mobile') == false) ? 'selected="selected"' : ''}}>Disuse</option>
-                    </select>
-                </dd>
-            </dl>
+        <div class="form-group">
+            <label for="page-option-comment">Comment</label>
+
+            <select id="page-option-comment" name="comment" class="form-control">
+                <option value="true" {{($config->get('comment') == true) ? 'selected="selected"' : ''}} >Use</option>
+                <option value="false" {{($config->get('comment') == false) ? 'selected="selected"' : ''}}>Disuse</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="page-option-mobile">Mobile Content</label>
+
+            <select id="page-option-mobile" name="mobile" class="form-control">
+                <option value="true" {{($config->get('mobile') == true) ? 'selected="selected"' : ''}} >Use</option>
+                <option value="false" {{($config->get('mobile') == false) ? 'selected="selected"' : ''}}>Disuse</option>
+            </select>
         </div>
     </div>
 </div>
+
 <input type="hidden" name="pageUid" value="{{$config->get('pageUid')}}"/>
