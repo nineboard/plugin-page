0527942b65a9a5109c15b4cec9e44b10c3caba6c
jhyeon1010 <jhyeon1010@naver.com>
2016-08-02 16:08:52 +0900

    #4 core editor 패키지 최신코드 반영

--- src/Controller/PageManageController.php
+++ src/Controller/PageManageController.php
@@ -18,6 +18,8 @@ use Request;
 use Redirect;
 use XePresenter;
 use XeEditor;
+use XeStorage;
+use XeTag;
 use App;
 use Xpressengine\Document\Models\Document;
 use Xpressengine\Plugins\Page\Module\Page as PageModule;
@@ -130,124 +132,14 @@ class PageManageController extends Controller
 
         $handler->updatePageContent($documentId, $pageId, $content, $title, $locale);
 
-        XeEditor::terminate($pageId, $documentId, Request::all());
+        $inputs = Request::all();
+        /** @var \Xpressengine\Editor\AbstractEditor $editor */
+        $editor = XeEditor::get($pageId);
+        // file 처리
+        XeStorage::sync($documentId, array_get($inputs, $editor->getFileInputName(), []));
+        // tag 처리
+        XeTag::set($documentId, array_get($inputs, $editor->getTagInputName(), []), $pageId);
 
         return Redirect::back();
     }
-//
-//    /**
-//     * file upload
-//     *
-//     * @return string|\Xpressengine\Presenter\RendererInterface
-//     * @throws \Xpressengine\Media\Exceptions\NotAvailableException
-//     * @throws \Xpressengine\Storage\Exceptions\InvalidFileException
-//     */
-//    public function fileUpload()
-//    {
-//        /** @var \Xpressengine\Storage\Storage $storage */
-//        $storage = app('xe.storage');
-//
-//        $uploadedFile = null;
-//        if (Request::file('file') !== null) {
-//            $uploadedFile = Request::file('file');
-//        } elseif (Request::file('image') !== null) {
-//            $uploadedFile = Request::file('image');
-//        }
-//
-//        if ($uploadedFile === null) {
-//            throw new \Exception;
-//        }
-//
-//        $file = $storage->upload($uploadedFile, PageModule::FILE_UPLOAD_PATH);
-//
-//        /** @var \Xpressengine\Media\MediaManager $mediaManager */
-//        $mediaManager = \App::make('xe.media');
-//        $media = null;
-//        $thumbnails = null;
-//
-//        if ($mediaManager->is($file) === true) {
-//            $media = $mediaManager->make($file);
-//            $thumbnails = $mediaManager->createThumbnails($media, PageModule::THUMBNAIL_TYPE);
-//
-//            $media = $media->toArray();
-//
-//            if (!empty($thumbnails)) {
-//                $info['thumbnails'] = $thumbnails;
-//            }
-//        }
-//
-//        return XePresenter::makeApi([
-//            'file' => $file->toArray(),
-//            'media' => $media,
-//            'thumbnails' => $thumbnails,
-//        ]);
-//    }
-//
-//    /**
-//     * 해시태그 suggestion 리스트
-//     *
-//     * @param string $url url
-//     * @param string $id  id
-//     * @return \Xpressengine\Presenter\RendererInterface
-//     */
-//    public function suggestionHashTag($url, $id = null)
-//    {
-//        /** @var \Xpressengine\Tag\TagHandler tag */
-//        $tag = \App::make('xe.tag');
-//        $terms = $tag->autoCompletion(\Request::get('string'));
-//
-//        $words = [];
-//        foreach ($terms as $tagEntity) {
-//            $words[] = $tagEntity->word;
-//        }
-//
-//        return XePresenter::makeApi($words);
-//    }
-//
-//    /**
-//     * 멘션 suggestion 리스트
-//     *
-//     * @param string $url url
-//     * @param string $id  id
-//     * @return \Xpressengine\Presenter\RendererInterface
-//     */
-//    public function suggestionMention($url, $id = null)
-//    {
-//        $userIds = [];
-//
-//        $string = Request::get('string');
-//
-//        /** @var \Xpressengine\User\Repositories\UserRepository $member */
-//        $member = app('xe.users');
-//
-//        // 10개 안되면 전체 DB 에서 찾아보자
-//        if (count($userIds) < 10) {
-//            $users = $member->query()->whereNotIn('id', $userIds)
-//                ->where('displayName', 'like', $string . '%')->get(['id']);
-//            foreach ($users as $user) {
-//                $userIds[] = $user['id'];
-//            }
-//        }
-//
-//        $users = $member->query()->whereIn('id', $userIds)
-//            ->where('displayName', 'like', $string . '%')->get(['id', 'displayName', 'profileImage']);
-//
-//        foreach ($users as $user) {
-//            $key = array_search($user['id'], $userIds);
-//            if ($key !== null && $key !== false) {
-//                unset($userIds[$key]);
-//            }
-//        }
-//
-//        // 본인은 안나오게 하자..
-//        $suggestions = [];
-//        foreach ($users as $user) {
-//            $suggestions[] = [
-//                'id' => $user['id'],
-//                'displayName' => $user['displayName'],
-//                'profileImage' => $user['profileImage'],
-//            ];
-//        }
-//        return XePresenter::makeApi($suggestions);
-//    }
 }
